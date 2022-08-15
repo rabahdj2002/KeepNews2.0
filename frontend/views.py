c52f8e6ee21f343613a497f9371a495618c14cae
Rabah Djebbes <39097998+rabahdj2002@users.noreply.github.com>
2022-08-15 22:03:38 +0100

    templates and static files

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(response):
+    return render(response, 'frontend/index.html', {})
