0a1cce462bca777032c7c32fdc39b1021d83c6a1
Rabah Djebbes <39097998+rabahdj2002@users.noreply.github.com>
2022-08-15 22:56:40 +0100

    create models

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsArticle(models.Model):
+    title = models.CharField(max_length=78)
+    author = models.CharField(max_length=60)
+    description = models.CharField(max_length=288)
+    url = models.CharField(max_length=500, null=True)
+    urlToImage = models.CharField(max_length=500, null=True)
+    publishedAt = models.DateField()
+    added = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ("-publishedAt",)
+
+    def __str__(self) -> str:
+        return self.title
+
+class SubscribersEmail(models.Model):
+    email = models.CharField(max_length=255)
+    created = models.DateField(auto_now_add=True)
+    sent = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.email
