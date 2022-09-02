e15124bab046e7f2816a686e4ef4ca8a75ac5ad3
Rabah Djebbes <39097998+rabahdj2002@users.noreply.github.com>
2022-09-02 19:29:52 +0100

    Update updater.py

--- jobs/updater.py
+++ jobs/updater.py
@@ -1,8 +1,10 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from .jobs import getNews
+from .jobs import getNews, keepAlive
 
 
 def start():
     scheduler = BackgroundScheduler()
     scheduler.add_job(getNews, 'interval', hours=1)
+    scheduler.add_job(keepAlive, 'interval', minutes=1)
     scheduler.start()
+
